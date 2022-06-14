4e2db03cd4ca480b60e6449f826106645313dec0
JZRodriguez <zujadiel@gmail.com>
2022-06-14 10:12:10 -0500

    processing

--- processing/processing.py
+++ processing/processing.py
@@ -1,11 +1,52 @@
-"""
-Script to process the data from the raw data and clean
-the csv files to upload them to the database.
-"""
-import pandas as pd
+#!/usr/bin/env python3
 
-def read_csv(file_name) -> pd.DataFrame:
-    """
-    Reads the csv file and returns a dataframe.
-    """
-    return pd.read_csv(file_name)
+import psycopg2
+from config import config
+import xml.etree.ElementTree as ET
+from datetime import date
+from datetime import datetime
+import matplotlib.pyplot as plt
+import subprocess
+
+
+def connect() :
+    conn = None
+    try :
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        print('PostgreSQL database version: ')
+        sql = "SELECT * from gpus"
+        cur.execute(sql)
+        records = cur.fetchall()
+        cur.close()
+
+        X = []
+        Y = []
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        for row in records :
+            Y.append(row[1])
+            date = datetime.date()
+            time = datetime.time()
+            datiteme = date + time
+            X.append(datiteme)
+
+        ax.plot(X, Y)
+        plt.savefig("/data/team3/gpus.png")
+        
+        process = subprocess.Popen(['scp','/data/team3/gpus.png','team3@10.99.1.138:/home/jzuniga/public_html/static/'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
+
+    except (Exception, psycopg2.DatabaseError) as error :
+        print(error)
+    finally :
+        if conn is not None :
+            conn.close()
+            print('Database connection closed.')
+
+if __name__ == '__main__' :
+    connect()
